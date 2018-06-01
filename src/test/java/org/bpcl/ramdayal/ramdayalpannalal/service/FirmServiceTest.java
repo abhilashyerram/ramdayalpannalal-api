8792b10a78859dc298f98e34551b07d2607f9484
Abhi <abhilash.4something@gmail.cm>
2018-06-01 17:12:50 -0400

    Junit test case for FirmService

--- src/test/java/org/bpcl/ramdayal/ramdayalpannalal/service/FirmServiceTest.java
+++ src/test/java/org/bpcl/ramdayal/ramdayalpannalal/service/FirmServiceTest.java
@@ -1,14 +1,53 @@
 package org.bpcl.ramdayal.ramdayalpannalal.service;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.bpcl.ramdayal.ramdayalpannalal.entity.FirmProfile;
+import org.bpcl.ramdayal.ramdayalpannalal.repository.FirmRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class FirmServiceTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@InjectMocks
+	private FirmService firmService;
+	private Iterable<FirmProfile> firms = new ArrayList<FirmProfile>();
+	private long firmId = 1;
+	
+	@Mock
+	FirmRepository firmRepository;
+	@Mock 
+	FirmProfile firm;
+	
+	private Optional<FirmProfile> optionalFirm;
+	
+	@Before
+	public void setUp() {
+		firmService = new FirmService();
+		((ArrayList<FirmProfile>) firms).add(firm);
+	}
+	
+	@Before
+	public void setUpMocks() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		when(firmRepository.findAll()).thenReturn(firms);
+		//when(firmRepository.findById(firmId)).thenReturn(optionalFirm);
 	}
 
+	@Test
+	public void getAllFirmsTest() throws Exception {
+		assertNotNull(firmService.getAllFirms());
+	}
+	
+	
+	
 }
