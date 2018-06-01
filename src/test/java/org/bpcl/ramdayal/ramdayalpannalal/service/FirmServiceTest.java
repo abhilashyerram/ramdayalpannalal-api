b9e6eafcc157ef74bfc4ee9b93262c7eeb4edc99
Abhi <abhilash.4something@gmail.cm>
2018-06-01 16:30:42 -0400

    firmById
    modified methodname to represent that it pulls by id

---
+++ src/test/java/org/bpcl/ramdayal/ramdayalpannalal/service/FirmServiceTest.java
@@ -0,0 +1,14 @@
+package org.bpcl.ramdayal.ramdayalpannalal.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class FirmServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
